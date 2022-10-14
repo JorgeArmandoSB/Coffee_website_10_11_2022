71547794752c353f63f73b5722dc77b22ef1d3ab
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-10-13 21:14:16 -0500

    website coffee shop finalizada

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert(`conectado`);
+let search = document.querySelector('.search-box');
+
+// efecto buscados
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+};
+
+// efecto header
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// efecto menu hamburguesa
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+};
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
